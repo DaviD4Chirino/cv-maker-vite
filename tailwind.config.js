3f191670e9cbb9b0393d613754bda13a2301a7df
David Space <genarodavid@gmail.com>
2024-07-07 02:23:38 -0400

    added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html,css}"],
   theme: {
     extend: {},
+    colors: {
+      text: "#e4f8f5",
+      background: "#030b0a",
+      primary: "#96e2d4",
+      secondary: "#253989",
+      accent: "#483eca",
+    },
   },
   plugins: [],
 };
